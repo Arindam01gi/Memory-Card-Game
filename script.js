e5f92b198295f6b322a3b0550431565118fedbc3
Arindam01gi <arindammaiti2018@gmail.com>
2022-10-24 20:26:11 +0530

    adding javascript test

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const cards = document.querySelectorAll(".card");
 
 // console.log(cards);
+
+let cardOne,cardTwo;
+
 function flipCard(event){
     let clickedCard = event.target;
     clickedCard.classList.add("flip");
+    if(!cardOne){
+       return  cardOne = clickedCard;
+    }
+    cardTwo = clickedCard;
+
+    console.log(cardOne,cardTwo);
+
 }
 
 cards.forEach(card =>{
