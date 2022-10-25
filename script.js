6afef7769e6bf855350ffafdce51cfabd0b3248e
Arindam01gi <arindammaiti2018@gmail.com>
2022-10-25 15:47:02 +0530

    Final Submission

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
 
 // console.log(cards);
 let matchedCard =0;
-let cardOne,cardTwo;
+let maxTime =20;
+let timeLeft = maxTime ;
+let cardOne,cardTwo,timer;
 let disableDeck = false;
+let isPlaying = false;
 
+
+function initTimer(){
+   if(timeLeft <=0 ){
+      return clearInterval(timer);
+   }
+   timeLeft--;
+   timeTag.innerText = timeLeft;
+}
 
 function flipCard(event){
     let clickedCard = event.target;
-   if(clickedCard!== cardOne && !disableDeck){
+
+    if(!isPlaying){
+      isPlaying=true;
+      timer = setInterval(initTimer,1000)
+    }
+
+
+   if(clickedCard!== cardOne && !disableDeck && timeLeft > 0 ){
+      flips++;
+      flipsTag.innerText = flips;
     clickedCard.classList.add("flip");
     if(!cardOne){
        return  cardOne = clickedCard;
@@ -24,10 +47,9 @@ function flipCard(event){
    function matchCards(img1,img2){
     if(img1 === img2){
         matchedCard++;
-        if(matchedCard==8){
-         setTimeout(()=>{
-            return   shuffleCard();
-         },1000);
+        if(matchedCard==6 && timeleft > 0){
+         return clearInterval(timer);
+        
         }
         cardOne.removeEventListener("click",flipCard);
         cardTwo.removeEventListener("click",flipCard);
@@ -43,15 +65,19 @@ function flipCard(event){
         cardTwo.classList.remove("shake", "flip");
         cardOne = cardTwo ="";
         disableDeck = false ; 
-     },800);
+     },1200);
 
    }
 
 function shuffleCard(){
-     matchedCard =0;
+     timeLeft = maxTime;
+     flips = matchedCard = 0;
      cardOne = cardTwo = "";
-     let arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
-     disableDeck = false ;
+     clearInterval(timer);
+     timeTag.innerText = timeLeft;
+     flipsTag.innerText = flips;
+     disableDeck = isPlaying = false;
+     let arr = [1,2,3,4,5,6,1,2,3,4,5,6];
      arr.sort(()=>Math.random()> 0.5 ? 1: -1);
      cards.forEach((card,index) =>{
         card.classList.remove("flip");
@@ -63,7 +89,8 @@ function shuffleCard(){
 }
 shuffleCard();
 
+refreshBtn.addEventListener("click", shuffleCard);
+
 cards.forEach(card =>{
-    
     card.addEventListener('click',flipCard);
 })
